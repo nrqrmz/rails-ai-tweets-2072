f81bc8c94d5bfd82f94bcc0c17649e716cad0d3b
Enrique Ramirez <nrqrmz@gmail.com>
2025-09-22 12:29:26 -0600

    add devise

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,26 @@
+class TweetsController < ApplicationController
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    @tweet.shortened = RubyLLM.chat.ask("Generate a tweet from this text: #{@tweet.long}").content
+    if @tweet.save
+      redirect_to tweet_path(@tweet), notice: "Tweet was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:long)
+  end
+end
